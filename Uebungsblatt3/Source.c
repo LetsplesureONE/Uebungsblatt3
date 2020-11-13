2eaece5e3116310ae0c1d76f9f3cbb3b0503cd32
LetsplesureONE <funlike1@gmail.com>
2020-11-13 12:16:54 +0100

    bis 2d

--- Uebungsblatt3/Source.c
+++ Uebungsblatt3/Source.c
@@ -4,17 +4,25 @@
 
 void aufgabe1a(void);
 void aufgabe1b(void);
+void aufgabe2a(void);
+void aufgabe2b(void);
+void aufgabe2c(void);
+void aufgabe2d(void);
 
 
 int main(void) {
 	//aufgabe1a();
-	aufgabe1b();
+	//aufgabe1b();
+	//aufgabe2a();
+	aufgabe2b();
+	//aufgabe2c();
+	//aufgabe2d();
 	return 0;
 }
 
 void aufgabe1a() {
-	char myArray[ARRAYSIZE] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	printArray(myArray, ARRAYSIZE);
+	char myArray[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	printArray(myArray, 10);
 }
 
 void aufgabe1b(void) {
@@ -30,4 +38,21 @@ void aufgabe1b(void) {
 	{ "          "} };
 	printMatrix(myMatrix1, 3, VALUE);
 	printMatrix(myMatrix2, 10, CHAR);
+}
+
+void aufgabe2a() {
+	printf("%d", myrand(256));
+}
+
+void aufgabe2b() {
+	int testarray[10];
+	randArray();
+}
+
+void aufgabe2c() {
+
+}
+
+void aufgabe2d() {
+
 }
